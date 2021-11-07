8cb5afe541fdb8fc36e5d8c2f6ad3df4c8f83148
Cedric Lorenz <cedric-lorenz@users.noreply.github.com>
2021-11-07 15:54:14 +0100

    fixes #4

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -5,15 +5,21 @@ describe "Author model", type: :model do
   first_name = "Alan"
   last_name = "Turing"
   homepage = "http://wikipedia.org/Alan_Turing"  
-  author = Author.new(first_name, last_name, homepage)
+  author = Author.new(first_name: first_name, last_name: last_name, homepage: homepage)
 
-  it "should have a first name, last name and homepage variable" do
+  it "should have a first name" do
     expect(author.first_name).to eq(first_name)
+  end
+
+  it "should have a last name" do
     expect(author.last_name).to eq(last_name)
+  end
+
+  it "should have a homepage" do
     expect(author.homepage).to eq(homepage)
   end
 
-  it "should have a name method" do
+  it "should have a name method which returns the full name" do
     expect(author.name).to eq(first_name + " " + last_name)
   end
 end
