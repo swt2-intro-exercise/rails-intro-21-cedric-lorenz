4741d0c726e8011fdab892a1bb89a9b3d45d3999
Cedric Lorenz <cedric-lorenz@users.noreply.github.com>
2021-11-10 17:56:14 +0100

    Fixes #6

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -23,11 +23,12 @@ describe "Author model", type: :model do
     expect(author.name).to eq(first_name + " " + last_name)
   end
 
-  it "should invalidate an author which has fields with the value nil" do
-    expect(@author).to_not be_valid
+  it "should invalidate an author whose last_name field has the value nil" do
+    no_last_name_author = Author.new(first_name: first_name, last_name: nil, homepage: homepage)
+    expect(no_last_name_author).to_not be_valid
   end
 
-   it "should validate an author whose fields are all not nil" do
+   it "should validate an author whose last_name field is not nil" do
     expect(author).to be_valid
   end
 end
