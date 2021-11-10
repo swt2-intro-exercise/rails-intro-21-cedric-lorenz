954331d80bb4323b825537025b9c19969d774ebb
Cedric Lorenz <cedric-lorenz@users.noreply.github.com>
2021-11-10 17:51:56 +0100

    Failing test for #6

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -22,4 +22,12 @@ describe "Author model", type: :model do
   it "should have a name method which returns the full name" do
     expect(author.name).to eq(first_name + " " + last_name)
   end
+
+  it "should invalidate an author which has fields with the value nil" do
+    expect(@author).to_not be_valid
+  end
+
+   it "should validate an author whose fields are all not nil" do
+    expect(author).to be_valid
+  end
 end
