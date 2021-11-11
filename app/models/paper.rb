c535f6846ce9abf35b5bfc70bd60f6edb056696b
Cedric Lorenz <cedric-lorenz@users.noreply.github.com>
2021-11-11 18:27:24 +0100

    Fixes #11

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -1,2 +1,4 @@
 class Paper < ApplicationRecord
+    validates :title, :venue, presence: true
+    validates :year, presence: true, numericality: true
 end
--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Paper, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  title = "Escalation"
+  venue = "Prosit"
+  year = 2018
+  paper = Paper.new(title: title, venue: venue, year: year)
+
+  it "should invalidate a paper who has a nil field" do
+    nil_paper= Paper.new
+    expect(nil_paper).to_not be_valid
+  end
+
+   it "should validate an paper that has no nil fields" do
+    expect(paper).to be_valid
+  end
 end
