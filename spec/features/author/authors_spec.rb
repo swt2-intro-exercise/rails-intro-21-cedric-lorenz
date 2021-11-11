fc72eecb81c96956187eec42d813a3fa28ab41a6
Cedric Lorenz <cedric-lorenz@users.noreply.github.com>
2021-11-11 17:58:11 +0100

    Fixes #9

--- spec/features/author/authors_spec.rb
+++ spec/features/author/authors_spec.rb
@@ -1,13 +1,21 @@
 require 'rails_helper'
 
 describe "Index author page", type: :feature do
-
-  it "should exist at 'authors_path' and render withour error" do
+  before do 
     visit authors_path
   end
 
+  it "should exist at 'authors_path' and render withour error" do
+  end
+
   it "should have link to 'new_author_path'" do
-    visit authors_path
     expect(page).to have_link('New', href: new_author_path)
+  end
+
+  it "should decrease the number of entries upon hitting the delete button of an entry" do
+    @author = Author.create(first_name: "Firsty", last_name: "Secondy", homepage: "https://example.com")
+    count = Author.count
+    @author.delete
+    expect(Author.count).to eq(count - 1)
   end
 end
